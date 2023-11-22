58552b1b766d7395e05271954f927ec7f2a6d3d3
skeltonmod <abgaoe@gmail.com>
2023-11-22 23:55:14 +0800

    remove shit css styles

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,23 +2,6 @@
   import { onMount } from "svelte";
 </script>
 
-<main class="center">
-  <div class="tui-window orange-168 black-255-text">
-    <fieldset class="tui-fieldset tui-border-solid white-border">
-      <legend class="center white-255-text">Window</legend>
-      <button class="tui-fieldset-button left"
-        ><span class="green-255-text">↑</span></button
-      >
-      <button class="tui-fieldset-button"
-        ><span class="green-255-text">■</span></button
-      >
-      Site under construction<br />
-      <br />
-      <div class="tui-divider"></div>
-      <br />
-      <br /><br />
-      <div class="tui-fieldset-text">Text format: UTF-8</div>
-      <div class="tui-fieldset-text right">Ln: 2, Col: 5</div>
-    </fieldset>
-  </div>
+<main>
+  
 </main>
